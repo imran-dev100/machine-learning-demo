b8327e3125ddc307b5b1d4890108d3e13ed02312
imran-dev100 <iagro7189@gmail.com>
2024-02-05 13:34:20 +0530

    predicting value 1200sqft house

--- Supervised-Machine-Learning:-Regression-and-Classification/Week-1/model_representation.py
+++ Supervised-Machine-Learning:-Regression-and-Classification/Week-1/model_representation.py
@@ -9,10 +9,8 @@ plt.style.use('/Users/imran/jupyter-workspace/machine-learning-demo/dependencies
 x_train = np.array([1.0,2.0])
 y_train = np.array([300.0,500.0])
 
-
 print(f"x_train = {x_train}")
 print(f"y_train = {y_train}")
-
 
 # identifying length of the training example array
 print(f"x_train.shape: {x_train.shape}")
