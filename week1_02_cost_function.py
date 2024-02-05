d90b2229a477054ba8b0c982e4d5400456b29bfa
imran-dev100 <iagro7189@gmail.com>
2024-02-05 16:10:41 +0530

    modifying_cost_function

--- week1_02_cost_function.py
+++ week1_02_cost_function.py
@@ -1,6 +1,4 @@
 import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use('/Users/imran/jupyter-workspace/machine-learning-demo/dependencies/deeplearning.mplstyle')
-
-
-from .lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+plt.style.use('./deeplearning.mplstyle')
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
