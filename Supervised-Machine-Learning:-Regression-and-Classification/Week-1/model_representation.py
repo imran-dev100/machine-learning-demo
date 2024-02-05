9291d53fc484af933fae9e00128a7a3b593cc9e0
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-02-05 12:23:29 +0530

    defining training examples

--- Supervised-Machine-Learning:-Regression-and-Classification/Week-1/model_representation.py
+++ Supervised-Machine-Learning:-Regression-and-Classification/Week-1/model_representation.py
@@ -2,3 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 # importing dependencies
 plt.style.use('/Users/imran/jupyter-workspace/machine-learning-demo/dependencies/deeplearning.mplstyle')
+
+# x_train is the input variable (size in 1000 square feet)
+# y_train is the target (price in 1000s of dollars)
+x_train = np.array([1.0,2.0])
+y_train = np.array([300.0,500.0])
