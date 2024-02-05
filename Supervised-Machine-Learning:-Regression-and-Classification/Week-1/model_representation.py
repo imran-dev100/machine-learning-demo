e2cf7a4c4426849af367394c5a196637ebe3ec89
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-02-05 12:28:29 +0530

    identifying length of the training example array

--- Supervised-Machine-Learning:-Regression-and-Classification/Week-1/model_representation.py
+++ Supervised-Machine-Learning:-Regression-and-Classification/Week-1/model_representation.py
@@ -5,8 +5,16 @@ plt.style.use('/Users/imran/jupyter-workspace/machine-learning-demo/dependencies
 
 # x_train is the input variable (size in 1000 square feet)
 # y_train is the target (price in 1000s of dollars)
+# creating array of numpy for training examples
 x_train = np.array([1.0,2.0])
 y_train = np.array([300.0,500.0])
 
+
 print(f"x_train = {x_train}")
 print(f"y_train = {y_train}")
+
+
+# identifying length of the training example array
+print(f"x_train.shape: {x_train.shape}")
+m = x_train.shape[0]
+print(f"Number of training examples is: {m}")
