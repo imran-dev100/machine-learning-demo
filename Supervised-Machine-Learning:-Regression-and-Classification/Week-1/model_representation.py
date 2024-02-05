9044e5f347a66661ed281c4e0ec490d8b48bc580
imran-dev100 <iagro7189@gmail.com>
2024-02-05 12:52:23 +0530

    Plot the data points

--- Supervised-Machine-Learning:-Regression-and-Classification/Week-1/model_representation.py
+++ Supervised-Machine-Learning:-Regression-and-Classification/Week-1/model_representation.py
@@ -18,3 +18,31 @@ print(f"y_train = {y_train}")
 print(f"x_train.shape: {x_train.shape}")
 m = x_train.shape[0]
 print(f"Number of training examples is: {m}")
+
+# iterate each training example individually
+for i in range(m):
+    x_i = x_train[i]
+    y_i = y_train[i]
+
+    print(f"(x^({i}), y^({i})) = ({x_i}, {y_i})")
+
+# plotting the data points of training set in graph
+plt.scatter(x_train, y_train, marker = 'x', c = 'r')
+
+# setting the title of graph
+plt.title("Housing prices")
+
+# setting the label of x-axis
+plt.xlabel("Size (in 1000 sq. ft.)")
+
+# setting the label of y-axis
+plt.ylabel("Price (in 1000s of USD)")
+
+# showing the plot in new tab
+plt.show()
+
+
+
+
+
+
