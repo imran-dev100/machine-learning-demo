da78d692122c7ef86a34d40d08d806a41fd09c41
imran-dev100 <111573254+imran-dev100@users.noreply.github.com>
2024-02-05 12:22:59 +0530

    importing dependencies

--- Supervised-Machine-Learning:-Regression-and-Classification/Week-1/model_representation.py
+++ Supervised-Machine-Learning:-Regression-and-Classification/Week-1/model_representation.py
@@ -1,2 +1,4 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# importing dependencies
+plt.style.use('/Users/imran/jupyter-workspace/machine-learning-demo/dependencies/deeplearning.mplstyle')
