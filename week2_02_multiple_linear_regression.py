a6daea13392a4793cf83e32aece4cd897a0ca1bc
imran-dev100 <iagro7189@gmail.com>
2024-02-07 08:05:30 +0530

    multiple linear regression.py

---
+++ week2_02_multiple_linear_regression.py
@@ -0,0 +1,23 @@
+import copy, math
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('./deeplearning.mplstyle')
+np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
+
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
+
+# data is stored in numpy array/matrix
+print(f"X Shape: {X_train.shape}, X Type:{type(X_train)})")
+print(X_train)
+print(f"\ny Shape: {y_train.shape}, y Type:{type(y_train)})")
+print(y_train)
+
+print(f"\nParameter vector w,b\n")
+
+b_init = 785.1811367994083
+w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+print(f"w_init shape: {w_init.shape}, w_init type: {type(w_init)}")
+
+print(f"b_init: {b_init}, b_init type: {type(b_init)}")
+
