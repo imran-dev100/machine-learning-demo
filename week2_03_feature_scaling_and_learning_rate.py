390d4394ef3d44641bb442ad366059a7e3f87218
imran-dev100 <iagro7189@gmail.com>
2024-02-08 10:01:40 +0530

    load the dataset

--- week2_03_feature_scaling_and_learning_rate.py
+++ week2_03_feature_scaling_and_learning_rate.py
@@ -5,3 +5,7 @@ from lab_utils_multi import  norm_plot, plt_equal_scale, plot_cost_i_w
 from lab_utils_common import dlc
 np.set_printoptions(precision=2)
 plt.style.use('./deeplearning.mplstyle')
+
+# load the dataset
+X_train, y_train = load_house_data()
+X_features = ['size(sqft)','bedrooms','floors','age']
